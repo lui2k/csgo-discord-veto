b93a04acddbe071dc00304c7ce0c8e639642e76a
lui2k <37487891+lui2k@users.noreply.github.com>
2018-03-17 23:55:29 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,80 +3,69 @@ const client = new Discord.Client();
 var maps;
 
 client.on('ready', () => {
-  console.log('I am ready!');
-  });
-  
-  client.on('message',message => {
-    if(message.content === '!startVeto' || message.content === '!resetVeto') {
-      message.reply('VetoStarting');
-      maps = ['cobble','cache', 'inferno','mirage','nuke','overpass','train'];
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '!startVeto' || message.content === '!resetVeto') {
+        message.reply('VetoStarting');
+        maps = 'cobble, cache, inferno, mirage, nuke, overpass, train';
     }
- });
- 
- client.login(process.env.BOT_TOKEN);
+});
+
+client.login(process.env.BOT_TOKEN);
 
 
-function remove(array, element) {
-  return array.filter(e => e !== element);
-}
-
-  message.reply("Pick a map to remove: " + maps);
-  
-  client.on('message',message => {
-    if(message.content === '!r cobble') {
-      maps = remove(maps,'cobble');
-      message.reply('Cobblestone removed. Maps left: ' + maps);
+client.on('message', message => {
+    if (message.content === '!r cobble') {
+        maps = maps.replace('cobble, ', '');
+        message.reply('Cobblestone removed. Maps left: ' + maps);
     }
- });
+});
 
-  client.on('message',message => {
-    if(message.content === '!r cache') {
-      maps = remove(maps,'cache');
-      message.reply('Cache removed. Maps left: ' + maps);
+client.on('message', message => {
+    if (message.content === '!r cache') {
+        maps = maps.replace('cache, ', '');
+        message.reply('Cache removed. Maps left: ' + maps);
     }
- });
-  
-  client.on('message',message => {
-    if(message.content === '!r mirage') {
-      maps = remove(maps,'mirage');
-      message.reply('Mirage removed. Maps left: ' + maps);
+});
+
+client.on('message', message => {
+    if (message.content === '!r mirage') {
+        maps = maps.replace('mirage, ', '');
+        message.reply('Mirage removed. Maps left: ' + maps);
     }
- });
-  
-    
-  client.on('message',message => {
-    if(message.content === '!r nuke') {
-      maps = remove(maps,'nuke');
-      message.reply('Nuke removed. Maps left: ' + maps);
+});
+
+
+client.on('message', message => {
+    if (message.content === '!r nuke') {
+        maps = maps.replace('nuke, ', '');
+        message.reply('Nuke removed. Maps left: ' + maps);
     }
- });
+});
 
 
 
-  client.on('message',message => {
-    if(message.content === '!r overpass') {
-      maps = remove(maps,'overpass');
-      message.reply('Overpass removed. Maps left: ' + maps);
+client.on('message', message => {
+    if (message.content === '!r overpass') {
+        maps = maps.replace('overpass, ', '');
+        message.reply('Overpass removed. Maps left: ' + maps);
     }
- });
+});
 
 
-  client.on('message',message => {
-    if(message.content === '!r train') {
-      maps = remove(maps,'train');
-      message.reply('Train removed. Maps left: ' + maps);
+client.on('message', message => {
+    if (message.content === '!r train') {
+        maps = maps.replace('train', '');
+        message.reply('Train removed. Maps left: ' + maps);
     }
- });
+});
 
 
-  client.on('message',message => {
-    if(message.content === '!r inferno') {
-      maps = remove(maps,'inferno');
-      message.reply('inferno removed. Maps left: ' + maps);
+client.on('message', message => {
+    if (message.content === '!r inferno') {
+        maps = maps.replace('inferno, ', '');
+        message.reply('inferno removed. Maps left: ' + maps);
     }
- });
-
-
-}
-
-}
+});

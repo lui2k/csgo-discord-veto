507a834c19184022438e429415500404e24760d9
lui2k <37487891+lui2k@users.noreply.github.com>
2018-03-18 00:55:32 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,21 +18,21 @@ client.login(process.env.BOT_TOKEN);
 
 
 client.on('message', message => {
-    if (message.content === '!r cobble' && maps.indexOf('cobble')!= -1) {
+    if (message.content.toLowerCase() === '!r cobble' && maps.indexOf('cobble')!= -1) {
         maps = maps.replace('cobble, ', '');
         message.reply('Cobblestone removed. Maps left: ' + maps);
     }
 });
 
 client.on('message', message => {
-    if (message.content === '!r cache') {
+    if (message.content.toLowerCase() === '!r cache' && maps.indexOf('cache')!= -1 {
         maps = maps.replace('cache, ', '');
         message.reply('Cache removed. Maps left: ' + maps);
     }
 });
 
 client.on('message', message => {
-    if (message.content === '!r mirage') {
+    if (message.content.toLowerCase() === '!r mirage' && maps.indexOf('mirage')!= -1) {
         maps = maps.replace('mirage, ', '');
         message.reply('Mirage removed. Maps left: ' + maps);
     }
@@ -40,7 +40,7 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-    if (message.content === '!r nuke') {
+    if (message.content.toLowerCase() === '!r nuke' && maps.indexOf('nuke')!= -1) {
         maps = maps.replace('nuke, ', '');
         message.reply('Nuke removed. Maps left: ' + maps);
     }
@@ -49,7 +49,7 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-    if (message.content === '!r overpass') {
+    if (message.content.toLowerCase() === '!r overpass' && maps.indexOf('overpass')!= -1) {
         maps = maps.replace('overpass, ', '');
         message.reply('Overpass removed. Maps left: ' + maps);
     }
@@ -57,7 +57,7 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-    if (message.content === '!r train') {
+    if (message.content.toLowerCase() === '!r train' && maps.indexOf('train')!= -1) {
         maps = maps.replace('train', '');
         message.reply('Train removed. Maps left: ' + maps);
     }
@@ -65,14 +65,24 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-    if (message.content === '!r inferno') {
+    if (message.content.toLowerCase() === '!r inferno' && maps.indexOf('inferno')!= -1 ) {
         maps = maps.replace('inferno, ', '');
         message.reply('inferno removed. Maps left: ' + maps);
     }
 });
 
+
+
 client.on('message',message=> {
     if(message.content === '!randomMap') {
      message.reply( items[Math.floor(Math.random()*items.length)] );
+    }
+});
+
+
+
+client.on('message', message => {
+    if (message.content.toLowerCase() === '!whatmaps') {
+        message.reply("Map Pool: Cobblestone, Cache, Inferno, Mirage, Nuke, Overpass, Train");
     }
 });

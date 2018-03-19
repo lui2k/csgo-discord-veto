18c83243b25846f49ab12e98e216e6374e0a4de2
lui2k <37487891+lui2k@users.noreply.github.com>
2018-03-19 10:52:44 +0000

    Update bot.js
    Change map removal from !r MAP  to  !veto MAP

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content.toLowerCase() === '!activedutyveto') {
         maps = 'cobble, cache, inferno, mirage, nuke, overpass, train';
-        message.reply('Active Duty Map Veto starting: Choose to ban any of the following maps: ' + maps);
+        message.reply('Active Duty Map Veto starting: Type !veto MapName to ban any of the following maps: ' + maps);
         mapsLeft = maps.split(",").length;
         allowBan=true;
     }
@@ -40,103 +40,147 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content.toLowerCase() === '!popflashveto') {
         maps = 'subzero, dust2, canals, cobble, cache, inferno, mirage, nuke, overpass, train';
-        message.reply('Popflash Map Veto starting: Choose to ban any of the following maps: ' + maps);
+        message.reply('Popflash Map Veto starting: Type !veto MapNam to ban any of the following maps: ' + maps);
         mapsLeft = maps.split(",").length;
         allowBan=true;
     }
 });
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r cobble' && maps.indexOf('cobble')!= -1 && allowBan) {
+    if (message.content.toLowerCase() === '!veto cobble' && maps.indexOf('cobble')!= -1 && allowBan) {
         maps = maps.replace('cobble, ', '');
         message.reply('Cobblestone removed. Maps left: ' + maps);
         mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r cache' && maps.indexOf('cache')!= -1 && allowBan) {
+    if (message.content.toLowerCase() === '!veto cache' && maps.indexOf('cache')!= -1 && allowBan) {
         maps = maps.replace('cache, ', '');
         mapsLeft = maps.split(",").length;
         message.reply('Cache removed. Maps left: ' + maps);
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r mirage' && maps.indexOf('mirage')!= -1 && allowBan) {
+    if (message.content.toLowerCase() === '!veto mirage' && maps.indexOf('mirage')!= -1 && allowBan) {
         maps = maps.replace('mirage, ', '');
         mapsLeft = maps.split(",").length;
         message.reply('Mirage removed. Maps left: ' + maps);
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r nuke' && maps.indexOf('nuke')!= -1 && allowBan) {
+    if (message.content.toLowerCase() === '!veto nuke' && maps.indexOf('nuke')!= -1 && allowBan) {
         maps = maps.replace('nuke, ', '');
         message.reply('Nuke removed. Maps left: ' + maps);
         mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
 
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r overpass' && maps.indexOf('overpass')!= -1 && allowBan) {
+    if (message.content.toLowerCase() === '!veto overpass' && maps.indexOf('overpass')!= -1 && allowBan) {
         maps = maps.replace('overpass, ', '');
         message.reply('Overpass removed. Maps left: ' + maps);
         mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r train' && maps.indexOf('train')!= -1 && allowBan) {
+    if (message.content.toLowerCase() === '!veto train' && maps.indexOf('train')!= -1 && allowBan) {
         maps = maps.replace('train', '');
         message.reply('Train removed. Maps left: ' + maps);
         mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r inferno' && maps.indexOf('inferno')!= -1 && allowBan ) {
+    if (message.content.toLowerCase() === '!veto inferno' && maps.indexOf('inferno')!= -1 && allowBan ) {
         maps = maps.replace('inferno, ', '');
         message.reply('Inferno removed. Maps left: ' + maps);
         mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r canals' && maps.indexOf('canals')!= -1 &&allowBan) {
+    if (message.content.toLowerCase() === '!veto canals' && maps.indexOf('canals')!= -1 &&allowBan) {
         maps = maps.replace('canals, ', '');
         message.reply('Canals removed. Maps left: ' + maps);
         mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r subzero' && maps.indexOf('subzero')!= -1 &&allowBan) {
+    if (message.content.toLowerCase() === '!veto subzero' && maps.indexOf('subzero')!= -1 &&allowBan) {
         maps = maps.replace('subzero, ', '');
         message.reply('Subzero removed. Maps left: ' + maps);
         mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
         
     }
 });
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r dust2' && maps.indexOf('dust2')!= -1 &&allowBan) {
+    if (message.content.toLowerCase() === '!veto dust2' && maps.indexOf('dust2')!= -1 &&allowBan) {
         maps = maps.replace('dust2, ', '');
         message.reply('Dust 2 removed. Maps left: ' + maps);
         mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!r dust 2' && maps.indexOf('dust2')!= -1 &&allowBan) {
+    if (message.content.toLowerCase() === '!veto dust 2' && maps.indexOf('dust2')!= -1 &&allowBan) {
         maps = maps.replace('dust2, ', '');
         message.reply('Dust 2 removed. Maps left: ' + maps);
-        mapsLeft = maps.split(",").length;        
+        mapsLeft = maps.split(",").length;
+        if(mapsLeft==1)
+        {
+            message.reply("Map left: " + maps);
+        }
     }
 });
 
